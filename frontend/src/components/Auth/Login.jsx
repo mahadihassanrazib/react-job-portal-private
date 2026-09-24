0a8bc722459a25b2e3326d186b2f66d85393d0a5
Hassan Razib <hassan.razib@bjitgroup.com>
2026-09-24 09:41:12 +0600

    login page update v2

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
         <div className="container">
           <div className="header">
             <img src="/careerconnect-black.png" alt="logo" />
-            <h3>Login to your account - Jenkins webhook test again</h3>
+            <h3>Login to your account - Jenkins webhook test again again</h3>
           </div>
           <form>
             <div className="inputTag">
